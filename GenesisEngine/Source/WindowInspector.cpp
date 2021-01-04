95c655a8a3f2750afd3fa99c958f44c254cb7210
pgalmor <47526823+PoleteTomaPedete@users.noreply.github.com>
2021-01-04 19:02:50 +0100

    add components from inspector

--- GenesisEngine/Source/WindowInspector.cpp
+++ GenesisEngine/Source/WindowInspector.cpp
@@ -24,6 +24,52 @@ void WindowInspector::Draw()
 		if (ImGui::CollapsingHeader("Resources")) {
 			App->resources->OnEditor();
 		}
+
+		ImGui::Separator();
+		if (ImGui::Button("Add Component..."))
+		{
+			ImGui::OpenPopup("AddPopUp");
+		}
+		if (ImGui::BeginPopup("AddPopUp"))
+		{
+			if (ImGui::MenuItem("Material..."))
+			{
+				if (App->scene->selectedGameObject->GetComponent(ComponentType::MATERIAL) == nullptr)
+				{
+					App->scene->selectedGameObject->AddComponent(ComponentType::MATERIAL);
+				}
+				ImGui::CloseCurrentPopup();
+			}
+			if (ImGui::MenuItem("Audio Emitter..."))
+			{
+				if (App->scene->selectedGameObject->GetComponent(ComponentType::AUDIO_EMITTER) == nullptr)
+				{
+					App->scene->selectedGameObject->AddComponent(ComponentType::AUDIO_EMITTER);
+				}
+				ImGui::CloseCurrentPopup();
+			}
+			if (ImGui::MenuItem("Audio Listener..."))
+			{
+				if (App->scene->selectedGameObject->GetComponent(ComponentType::AUDIO_LISTENER) == nullptr)
+				{
+					App->scene->selectedGameObject->AddComponent(ComponentType::AUDIO_LISTENER);
+				}
+				ImGui::CloseCurrentPopup();
+			}
+			/*else
+			{
+				if (ImGui::IsMouseClicked(0))
+				{
+					atLOG("This GameObject already has a Component Material!");
+					ImGui::CloseCurrentPopup();
+				}
+				if (ImGui::MenuItem("Material...", 0, false, false))
+				{
+					ImGui::CloseCurrentPopup();
+				}
+			}*/
+			ImGui::EndPopup();
+		}
 	}
 	ImGui::End();
 }
